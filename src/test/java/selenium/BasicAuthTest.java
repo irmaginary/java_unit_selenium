765b3bcfc7656d08ea13a0e10d37f5ed4914dbec
irmasolano <i.solano.baca@gmail.com>
2024-04-05 17:20:23 -0600

    Test passed (with some warnings)2

--- src/test/java/selenium/BasicAuthTest.java
+++ src/test/java/selenium/BasicAuthTest.java
@@ -1,7 +1,10 @@
 package selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicAuthTest extends BaseTest {
@@ -9,7 +12,12 @@ public class BasicAuthTest extends BaseTest {
     private final By SUCCESS_AUTH = By.xpath(String.format(PARTICULAR_TEXT_XPATH,
             "Congratulations! You must have the proper credentials"));
 
-    //todo: add basic auth
+
+    @BeforeMethod
+    public void baseAuth() {
+        HasAuthentication authentication = (HasAuthentication) driver;
+        authentication.register(() -> new UsernameAndPassword("admin", "admin"));
+    }
 
     @Test
     public void basicAuthTest() {
